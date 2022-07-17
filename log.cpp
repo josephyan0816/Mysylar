7bec4f89db7d19eb86f890a89b25ffb351aec81e
josephyan <387717545@qq.com>
2022-07-17 22:28:11 +0800

    feat(log):FileLogAppender and its implement

--- log.cpp
+++ log.cpp
@@ -41,4 +41,26 @@ namespace sylar{
         fatal(LogLevel::FATAL,event);
     }
 
+    FileLogAppender::FileLogAppender(const std::string &filename):m_filename(filename) {
+
+    }
+    void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event) {
+        if(level>=m_level){
+            m_filestream<<m_formatter->format(event);
+        }
+    }
+    bool FileLogAppender::reopen() {
+        if(m_filestream){
+            m_filestream.close();
+        }
+        m_filestream.open(m_filename);
+        return !!m_filestream;
+
+    }
+    void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event){
+        if(level>=m_level){
+            std::cout<<m_formatter.format(event);
+        }
+    }
+
 }
--- log.h
+++ log.h
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <memory>
 #include <list>
+#include <fstream>
+#include <sstream>
 
 namespace sylar {
 
@@ -21,7 +23,7 @@ namespace sylar {
     private:
         const char *m_file = nullptr;//文件名
         int32_t m_line = 0;//行号
-        uint32_t e_elapse=0;//程序启动到现在的毫秒数
+        uint32_t e_elapse = 0;//程序启动到现在的毫秒数
         int32_t m_threadId = 0;//线程id
         uint32_t m_fiberId = 0;//协程id
         uint64_t m_time;//时间戳
@@ -45,9 +47,12 @@ namespace sylar {
     class LogFormatter {
     public:
         typedef std::shared_ptr<LogFormatter> ptr;
+
         std::string format(LogEvent::ptr event);
+
     private:
     };
+
     //日志输出地
     class LogAppender {
     public:
@@ -55,10 +60,19 @@ namespace sylar {
 
         virtual ~LogAppender() {}
 
-        void log(LogLevel::Level level,LogEvent::ptr event);
+        virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
 
-    private:
+        void setFormatter(LogFormatter::ptr val) {
+            m_formatter = val;
+        }
+
+        LogFormatter::ptr getFormatter() const {
+            return m_formatter;
+        }
+
+    protected:
         LogLevel::Level m_level;
+        LogFormatter::ptr m_formatter;
     };
 
 
@@ -74,28 +88,53 @@ namespace sylar {
         void log(LogLevel::Level level, const LogEvent::ptr event);
 
         void debug(LogEvent::ptr event);
+
         void info(LogEvent::ptr event);
+
         void warn(LogEvent::ptr event);
+
         void fatal(LogEvent::ptr event);
+
         void error(LogEvent::ptr event);
+
         void addAppender(LogAppender::ptr appender);
+
         void delAppender(LogAppender::ptr appender);
-        LogLevel::Level getLevel() const{ return m_level;}
-        void setLevel(LogLevel::Level val){m_level= val;}
+
+        LogLevel::Level getLevel() const { return m_level; }
+
+        void setLevel(LogLevel::Level val) { m_level = val; }
 
     private:
         std::string m_name;//日志名称
         LogLevel::Level m_level;//日志级别
-       std::list<LogAppender::ptr>  m_appenders;//Appender集合
+        std::list<LogAppender::ptr> m_appenders;//Appender集合
     };
 
     //输出到控制台的Appender
-    class StdoutLogAppender:public LogAppender{
+    class StdoutLogAppender : public LogAppender {
+    public:
+        typedef std::shared_ptr<StdoutLogAppender> ptr;
+
+        void log(LogLevel::Level level, LogEvent::ptr event) override;
 
     };
-    //输出到文件的appender
-    class FileLogAppender:public LogAppender{
 
+    //输出到文件的appender
+    class FileLogAppender : public LogAppender {
+    public:
+        typedef std::shared_ptr<FileLogAppender> ptr;
+
+        FileLogAppender(const std::string &filename);
+
+        void log(LogLevel::Level level, LogEvent::ptr event) override;
+
+        //重新打开文件，文件打开成功返回true
+        bool reopen();
+
+    private:
+        std::string m_filename;
+        std::ofstream m_filestream;
     };
 
 }
